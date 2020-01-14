85fa74d61248955ec49fabcbfaff52767c1d271e
dnyaneshwar <dnyaneshwar.sukhashe@iauro.com>
2020-01-14 09:30:33 +0530

    Changes made by Kiran

--- src/app/sat-tv-component/sat-tv-home/customer-details/customer-details.component.ts
+++ src/app/sat-tv-component/sat-tv-home/customer-details/customer-details.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerDetailsComponent implements OnInit {
 
+  person =[
+    {
+      id:101,
+      name:"Kiran",
+      MiddleName:"Annasaheb",
+      LastName:"Lasure"
+    },
+    {
+      id:102,
+      name:"Tushar",
+      MiddleName:"Annasaheb",
+      LastName:"Lasure"
+    },
+    {
+      id:103,
+      name:"sonali",
+      MiddleName:"Tushar",
+      LastName:"Lasure"
+    },
+    {
+      id:104,
+      name:"Aniket",
+      MiddleName:"Balu",
+      LastName:"Lasure"
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
   }
+  
+  check(data1){
+    localStorage.setItem("data",data1); 
+    console.log(data1);
+ }
+  getData()
+  {
 
+    // let js=JSON.stringify("data");
+    // console.log();
+    // console.log(localStorage.getItem(js));
+  }
 }
